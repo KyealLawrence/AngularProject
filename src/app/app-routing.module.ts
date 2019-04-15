6baa8b954d91b474a85e89cc8b1d0000768098ec
Kyeal <kyeal1995@hotmail.com>
2019-04-15 13:31:02 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NewpageComponent } from './newpage/newpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path : 'newpage',
+		component : NewpageComponent  
+	}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from '@angular/fire';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { environment } from '../environments/environment';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { NewpageComponent } from './newpage/newpage.component'; 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewpageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AngularFireModule.initializeApp(environment.firebase),
+    AppRoutingModule,
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
